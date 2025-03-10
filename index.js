605947cea78db524989ff5699abebb12ce00394a
Chethana Potukanam <chethanapotukanam@gmail.com>
2025-03-10 06:54:33 +0530

    Add get route in index.js

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ app.use(cors());
 app.use("/posts", postRoutes);
 app.use("/users", userRoutes);
 
+app.get('/',(req, res)=>{
+    res.send("APP is running successfully");
+})
+
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
